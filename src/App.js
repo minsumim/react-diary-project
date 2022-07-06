0049a7b4f5a9ab512b6161d20861b944317c460d
mimmel <mfblade2014@gmail.com>
2022-07-06 17:59:43 +0900

    DiaryList add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {useState, useEffect} from 'react';
 import './App.css';
 import DiaryEditer from './DiaryEditer';
+import DiaryList from './DiaryList';
 
 function App() {
   return (
     <div className="App">
       <DiaryEditer/>
+      <DiaryList />
     </div>
   );
 }
